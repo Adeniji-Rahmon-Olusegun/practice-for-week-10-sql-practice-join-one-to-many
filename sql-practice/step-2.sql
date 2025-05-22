4f51b0a26b231b66ea126b24f280968a283e3ff7
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2025-05-22 17:48:12 +0100

    update

--- sql-practice/step-2.sql
+++ sql-practice/step-2.sql
@@ -3,7 +3,7 @@
 -- final output of what is being SELECTed for.
 -- Your code here
 
-SELECT bands.name, albums.title 
+SELECT bands.name
 FROM bands
 JOIN albums ON (bands.id = albums.band_id)
 WHERE albums.num_sold < 20000;
